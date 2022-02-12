ced91e1d5703e02a3759345fc67868a5fa44378f
Philoso-Raptor <99517874+Philoso-Raptor@users.noreply.github.com>
2022-02-12 16:17:04 -0600

    Osmantium Ingot and Nugget
    Addition of Osmantium Ingot and Nugget with future plans for blocks.

--- src/main/java/net/raptorratmod/randommod/RandomMod.java
+++ src/main/java/net/raptorratmod/randommod/RandomMod.java
@@ -1,6 +1,7 @@
 package net.raptorratmod.randommod;
 
 import net.fabricmc.api.ModInitializer;
+import net.raptorratmod.randommod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +9,17 @@ public class RandomMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	public static final String MOD_ID = "randommod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 //This Should be viewable and should be in package net.raptorratmod.randommod
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/raptorratmod/randommod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.raptorratmod.randommod.item;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.raptorratmod.randommod.RandomMod;
+
+public class ModItems {
+
+    public static final Item OSMANTIUM_INGOT = registerItem("osmantium_ingot"
+    ,new Item(new FabricItemSettings().fireproof().group(ItemGroup.MISC)));
+
+    public static final Item OSMANTIUM_NUGGET = registerItem("osmantium_nugget"
+            ,new Item(new FabricItemSettings().fireproof().group(ItemGroup.MISC)));
+
+
+
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registry.ITEM, new Identifier(RandomMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        RandomMod.LOGGER.info("Registering Mod Items for " + RandomMod.MOD_ID);
+
+    }
+}
