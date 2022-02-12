436b541b3ce21d7e79ecb05eacbc48b32c561f99
Tate Armstrong <90807397+TateArmstrong@users.noreply.github.com>
2022-02-11 19:58:44 -0600

    Create ExampleMod.java

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -9,10 +9,11 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-	// WHAT THE FUCK RAT!!!!!!
+	
 	// WHAT THE FUCK GRAT!!!!!!
 	// HOLY COW!!!!
 	// HOLY COW X2 !!!!!
+	// HOLY COW X3 !!!!!
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
