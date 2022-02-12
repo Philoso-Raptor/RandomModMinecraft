efe6d05c6e2588696a37fd15c32a74bad9336810
Philoso-Raptor <99517874+Philoso-Raptor@users.noreply.github.com>
2022-02-11 19:56:59 -0600

    Update ExampleMod.java

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -9,7 +9,14 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-	
+	// WHAT THE FUCK RAT!!!!!!
+	// WHAT THE FUCK RAT!!!!!!
+	// WHAT THE FUCK RAT!!!!!!
+	// WHAT THE FUCK RAT!!!!!!
+	// WHAT THE FUCK RAT!!!!!!
+	// WHAT THE FUCK RAT!!!!!!
+	// WHAT THE FUCK RAT!!!!!!
+	// WHAT THE FUCK RAT!!!!!!
 	// WHAT THE FUCK GRAT!!!!!!
 	// HOLY COW!!!!
 	@Override
