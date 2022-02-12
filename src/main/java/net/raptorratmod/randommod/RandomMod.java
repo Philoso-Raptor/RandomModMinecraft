b6e6dd31f2f9c27fab0b70555d198016c4a2443c
Philoso-Raptor <99517874+Philoso-Raptor@users.noreply.github.com>
2022-02-12 13:10:38 -0600

    Double Checking the save was Successful
    Title

--- src/main/java/net/raptorratmod/randommod/RandomMod.java
+++ src/main/java/net/raptorratmod/randommod/RandomMod.java
@@ -9,7 +9,7 @@ public class RandomMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
+//This Should be viewable and should be in package net.raptorratmod.randommod
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
