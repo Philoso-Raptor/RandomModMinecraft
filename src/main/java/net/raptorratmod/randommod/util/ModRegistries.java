d837fb2f475ab2424e286339faba35b9ce6a4871
Philoso-Raptor <99517874+Philoso-Raptor@users.noreply.github.com>
2022-05-11 23:47:34 -0500

    Block Entity
    block entity stuff

--- src/main/java/net/raptorratmod/randommod/util/ModRegistries.java
+++ src/main/java/net/raptorratmod/randommod/util/ModRegistries.java
@@ -3,7 +3,6 @@ package net.raptorratmod.randommod.util;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.raptorratmod.randommod.entity.ModEntities;
 import net.raptorratmod.randommod.entity.custom.JJEntity;
-import net.raptorratmod.randommod.entity.custom.WWEntity;
 
 public class ModRegistries {
     public static void registerModStuffs(){
@@ -12,7 +11,6 @@ public class ModRegistries {
     }
     private static void registerAttributes(){
         FabricDefaultAttributeRegistry.register(ModEntities.JJ, JJEntity.setAttributes());
-        FabricDefaultAttributeRegistry.register(ModEntities.WEREWOLF, WWEntity.setAttributes());
     }
 
 
